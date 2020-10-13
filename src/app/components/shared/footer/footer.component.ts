83806e7ec5808fc773366f051ff2e4ecffbb3b8a
carlos mur <cmurestudillos@gmail.com>
2020-10-13 15:10:06 +0200

    commit

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -7,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  year = new Date().getFullYear();
-
   constructor() { }
 
   ngOnInit() {
